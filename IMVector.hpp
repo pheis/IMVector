187d0e44d6bed2626c66c4ae08addf2dc4f69792
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2017-11-26 00:59:38 +0200

    Implement push_back, set, get

---
+++ IMVector.hpp
@@ -0,0 +1,211 @@
+#include <array>
+#include <cstddef>
+#include <iostream>
+#include <memory>
+#include <utility>
+
+// M == branching factor,
+// tells how many children there are for a branch and a leaf
+
+const uint32_t B = 4;
+const uint32_t M = 1 << B;
+
+// gotta be fast.
+// same thing could be achieved with / and %,
+// but I believe this is faster.
+uint32_t local_idx(uint32_t idx, uint32_t lvls) {
+  return (idx >> (B * lvls)) & (M - 1);
+}
+
+using std::array;
+using std::cout;
+using std::endl;
+using std::make_shared;
+using std::make_unique;
+using std::move;
+using std::shared_ptr;
+using std::string;
+using std::unique_ptr;
+
+template <class T> class Node {
+public:
+  virtual shared_ptr<Node<T>> set(uint32_t idx, uint32_t levels, T some) = 0;
+  virtual T get(uint32_t idx, uint32_t levels) = 0;
+  virtual void destructive_set(uint32_t idx, uint32_t lvls, T some) = 0;
+};
+
+template <class T> class Branch : public Node<T> {
+public:
+  explicit Branch() { contents = array<shared_ptr<Node<T>>, M>(); }
+
+  explicit Branch(array<shared_ptr<Node<T>>, M> c) : contents(c) {}
+
+  explicit Branch(shared_ptr<Node<T>> &old_root) {
+    contents = array<shared_ptr<Node<T>>, M>();
+    // this is kinda ugly
+    for (uint32_t i = 0; i < M; i++)
+      contents[i] = old_root;
+  }
+
+  T get(uint32_t idx, uint32_t lvls) {
+    uint32_t loc_idx = local_idx(idx, lvls);
+    return contents[loc_idx]->get(idx, lvls - 1);
+  }
+
+  shared_ptr<Node<T>> set(uint32_t idx, uint32_t lvls, T some) {
+
+    shared_ptr<Branch<T>> new_branch = make_shared<Branch<T>>(contents);
+
+    uint32_t l_idx = local_idx(idx, lvls);
+
+    new_branch->contents[l_idx] = contents[l_idx]->set(idx, lvls - 1, some);
+
+    return new_branch;
+  }
+
+  void destructive_set(uint32_t idx, uint32_t lvls, T some) {
+    contents[local_idx(idx, lvls)]->destructive_set(idx, lvls - 1, some);
+  }
+
+  array<shared_ptr<Node<T>>, M> contents;
+};
+
+template <class T> class Leaf : public Node<T> {
+public:
+  explicit Leaf() { contents = array<T, M>(); }
+  explicit Leaf(array<T, M> c) : contents(c) {}
+
+  T get(uint32_t idx, uint32_t lvls) { return contents[local_idx(idx, lvls)]; }
+
+  shared_ptr<Node<T>> set(uint32_t idx, uint32_t lvls, T some) {
+
+    shared_ptr<Leaf<T>> new_leaf = make_shared<Leaf<T>>(Leaf<T>(contents));
+
+    new_leaf->contents[local_idx(idx, lvls)] = some;
+
+    return new_leaf;
+  }
+
+  void destructive_set(uint32_t idx, uint32_t lvls, T some) {
+    contents[local_idx(idx, lvls)] = some;
+  }
+
+  array<T, M> contents;
+};
+
+template <class T> class IMVector {
+public:
+  explicit IMVector<T>() {
+    root = nullptr;
+    size = 0;
+    lvls = 0;
+  }
+
+  explicit IMVector<T>(shared_ptr<Node<T>> r, uint32_t s, uint32_t l)
+      : root(r), size(s), lvls(l) {}
+
+  explicit IMVector<T>(int s) {
+    if (s == 0) {
+      root = nullptr;
+      size = 0;
+      lvls = 0;
+    } else if (s < M) {
+      root = make_shared<Leaf<T>>(Leaf<T>());
+      size = s;
+      lvls = 1;
+    } else {
+      uint32_t level_count = 1;
+      uint32_t x = s;
+      while (x > 0) {
+        x = B >> x;
+        level_count++;
+      }
+
+      // works//
+      shared_ptr<Leaf<T>> new_root = make_shared<Leaf<T>>(Leaf<T>());
+      shared_ptr<Node<T>> r = new_root;
+      for (int i = 1; i < level_count; i++)
+        r = make_shared<Branch<T>>(Branch<T>(r));
+      // don't touch... //
+
+      root = r;
+      size = s;
+      lvls = level_count;
+    }
+  }
+
+  uint32_t levels() { return lvls; }
+  uint32_t get_size() { return size; }
+
+  IMVector<T> push_back(T some) {
+
+    // cout << "starting push_back" << endl;
+
+    uint32_t new_size = size + 1;
+    uint32_t idx = size;
+
+    if (1 << (B * lvls) == new_size) {
+
+      // cout << " must make greater size" << endl;
+      // cout << "LVLS:" << lvls << endl;
+      // cout << "1 << B * lvls: " << (1 << (B * lvls)) << endl;
+
+      uint32_t new_lvls = lvls + 1; // if and only if needed
+
+      shared_ptr<Node<T>> new_root = grow();
+
+      IMVector new_vector(new_root, new_size, new_lvls);
+
+      // cout << "n size " << new_vector.get_size() << endl;
+
+      // cout << "n lvls " << new_vector.levels() << endl;
+
+      new_vector.destructive_set(idx, some);
+
+      return new_vector;
+    } else {
+      // cout << "there was enough levels" << endl;
+      shared_ptr<Node<T>> new_root = root->set(idx, lvls - 1, some);
+      // cout << "set was OK" << endl;
+      return IMVector(new_root, new_size, lvls);
+    }
+  }
+
+  T get(uint32_t idx) { return root->get(idx, lvls - 1); }
+
+  IMVector<T> set(int idx, T some) {
+    shared_ptr<Node<T>> new_root = root->set(idx, lvls - 1, some);
+    return IMVector(new_root, size, lvls);
+  }
+
+  void destructive_set(uint32_t idx, T some) {
+    root->destructive_set(idx, lvls - 1, some);
+  }
+
+  shared_ptr<Node<T>> grow() {
+    if (lvls != 0) {
+      // cout << "Growing, new root is a Branch" << endl;
+
+      // does this shit work;
+      shared_ptr<Branch<T>> new_root = make_shared<Branch<T>>(Branch<T>(root));
+
+      return new_root;
+
+    } else {
+      shared_ptr<Leaf<T>> new_root = make_shared<Leaf<T>>(Leaf<T>());
+      return new_root;
+    }
+  }
+
+  // shared_ptr<Node<T>> shrink() {}
+  void print_contents() {
+    for (int i = 0; i < size; i++) {
+      std::cout << get(i) << std::endl;
+    }
+  }
+
+private:
+  shared_ptr<Node<T>> root;
+  uint32_t size;
+  uint32_t lvls;
+};
---
+++ main.cpp
@@ -0,0 +1,133 @@
+#include "IMVector.hpp"
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+int AMOUNT = 10000;
+
+int main() {
+  auto start = std::chrono::system_clock::now();
+  IMVector<int> v = IMVector<int>(100000);
+  for (int i = 0; i < 100000; i++) {
+    v = v.set(i, i);
+    // cout << "CONTENTS" << endl;
+    // cout << "END" << endl << endl << endl;
+  }
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  cout << elapsed_seconds.count() << endl;
+}
+// v.print_contents();
+
+//   IMVector<int> v2 = IMVector<int>(10000);
+//   // IMVector<int> v2 = IMVector<int>();
+//
+//   // for (int i = 0; i < 100; i++) {
+//
+//   //  v2 = v2.push_back(i);
+//   //}
+//
+//   for (int i = 0; i < 100; i++) {
+//     v2 = v2.set(i, i);
+//   }
+//
+//   v2.print_contents();
+// }
+/*
+auto start2 = std::chrono::system_clock::now();
+std::vector<int> ve = std::vector<int>();
+for (int i = 0; i < AMOUNT; i++) {
+  ve.push_back(i);
+  // cout << "CONTENTS" << endl;
+  // cout << "END" << endl << endl << endl;
+}
+auto end2 = std::chrono::system_clock::now();
+std::chrono::duration<double> elapsed_seconds2 = end2 - start2;
+cout << elapsed_seconds2.count() << endl;
+
+cout << endl;
+cout << endl;
+cout << endl;
+cout << endl;
+
+auto t1 = elapsed_seconds.count();
+auto t2 = elapsed_seconds2.count();
+
+cout << t1 / t2 << endl;
+
+auto start3 = std::chrono::system_clock::now();
+std::unordered_map<int, int> um = std::unordered_map<int, int>();
+for (int i = 0; i < AMOUNT; i++) {
+  um[i] = i;
+  // cout << "CONTENTS" << endl;
+  // cout << "END" << endl << endl << endl;
+}
+auto end3 = std::chrono::system_clock::now();
+std::chrono::duration<double> elapsed_seconds3 = end3 - start3;
+cout << elapsed_seconds3.count() << endl;
+
+auto t3 = elapsed_seconds3.count();
+
+cout << t1 / t3 << endl;
+
+auto start4 = std::chrono::system_clock::now();
+std::vector<std::pair<int, int>> vp = std::vector<std::pair<int, int>>();
+for (int i = 0; i < 100000; i++) {
+  vp.push_back(std::make_pair(i, i));
+  // cout << "CONTENTS" << endl;
+  // cout << "END" << endl << endl << endl;
+}
+auto end4 = std::chrono::system_clock::now();
+std::chrono::duration<double> elapsed_seconds4 = end4 - start4;
+cout << elapsed_seconds4.count() << endl;
+
+auto t4 = elapsed_seconds4.count();
+
+cout << t1 / t4 << endl;
+
+v = v.set(1, 12342);
+
+// v.print_contents();
+
+auto start5 = std::chrono::system_clock::now();
+IMVector<IMVector<int>> vv = IMVector<IMVector<int>>();
+for (int i = 0; i < 100; i++) {
+  IMVector<int> vvv = IMVector<int>();
+  vv = vv.push_back(vvv);
+  for (int j = 0; j < 100; j++) {
+
+    vv = vv.set(i, (vv.get(i)).push_back(j));
+  }
+}
+auto end5 = std::chrono::system_clock::now();
+std::chrono::duration<double> elapsed_seconds5 = end5 - start5;
+cout << elapsed_seconds5.count() << endl;
+
+auto t5 = elapsed_seconds5.count();
+
+cout << t1 / t5 << endl;
+
+auto start6 = std::chrono::system_clock::now();
+vector<vector<int>> asdf = vector<vector<int>>();
+for (int i = 0; i < 100; i++) {
+  vector<int> as = vector<int>();
+  asdf.push_back(as);
+  for (int j = 0; j < 100; j++) {
+    asdf[i].push_back(j);
+  }
+}
+auto end6 = std::chrono::system_clock::now();
+std::chrono::duration<double> elapsed_seconds6 = end6 - start6;
+cout << elapsed_seconds6.count() << endl;
+
+auto t6 = elapsed_seconds6.count();
+
+cout << t5 / t6 << endl;
+}
+*/
